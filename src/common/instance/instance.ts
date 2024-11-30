1eb734cc7fd25403e88c05ba62508df03d45115b
YurySamsonenka <cant09y@gmail.com>
2024-11-30 21:26:04 +0300

    13-s4-w1 add .env

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-	baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+	baseURL: process.env.REACT_APP_BASE_URL,
 	headers: {
-		Authorization: 'Bearer 2026887c-01d4-466b-bc2c-7397d8c89fb8',
-		'API-KEY': 'd9e3a3ee-9579-4622-9ab3-970dc7443157',
+		Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
+		'API-KEY': process.env.REACT_APP_API_KEY,
 	},
-})
+});
