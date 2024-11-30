e591aaa90dd2f1c25c8435b4cc74cdbe79e6f1bb
YurySamsonenka <cant09y@gmail.com>
2024-11-30 21:06:24 +0300

    13-s4-w1 instance tasks

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,50 +1,22 @@
-import axios from 'axios';
 import { CreateTaskResponse, DeleteTaskResponse, DomainTask, GetTasksResponse, UpdateTaskModel, UpdateTaskResponse } from './tasksApi.types';
+import { instance } from '../../../common/instance/instance';
 
 export const tasksApi = {
 	getTask(id: string) {
-		return axios.get<GetTasksResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}/tasks`,
-			{
-				headers: {
-					Authorization: 'Bearer 2026887c-01d4-466b-bc2c-7397d8c89fb8',
-					'API-KEY': 'd9e3a3ee-9579-4622-9ab3-970dc7443157',
-				},
-			});
+		return instance.get<GetTasksResponse>(`todo-lists/${id}/tasks`);
 	},
 	createTask(payload: { title: string, todolistId: string }) {
 		const { title, todolistId } = payload;
-		return axios.post<CreateTaskResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}/tasks`,
-			{ title },
-			{
-				headers: {
-					Authorization: 'Bearer 2026887c-01d4-466b-bc2c-7397d8c89fb8',
-					'API-KEY': 'd9e3a3ee-9579-4622-9ab3-970dc7443157',
-				},
-			});
+		return instance.post<CreateTaskResponse>(`todo-lists/${todolistId}/tasks`, { title });
 	},
 	removeTask(payload: { taskId: string, todolistId: string }) {
 		const { taskId, todolistId } = payload;
-		return axios.delete<DeleteTaskResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}/tasks/${taskId}`,
-			{
-				headers: {
-					Authorization: 'Bearer 2026887c-01d4-466b-bc2c-7397d8c89fb8',
-					'API-KEY': 'd9e3a3ee-9579-4622-9ab3-970dc7443157',
-				},
-			});
+		return instance.delete<DeleteTaskResponse>(`todo-lists/${todolistId}/tasks/${taskId}`);
 	},
 	updateTask(payload: { task: DomainTask, model: UpdateTaskModel }) {
 		const { task, model } = payload;
 
-		return axios
-			.put<UpdateTaskResponse>(
-				`https://social-network.samuraijs.com/api/1.1/todo-lists/${task.todoListId}/tasks/${task.id}`,
-				model,
-				{
-					headers: {
-						Authorization: 'Bearer 2026887c-01d4-466b-bc2c-7397d8c89fb8',
-						'API-KEY': 'd9e3a3ee-9579-4622-9ab3-970dc7443157',
-					},
-				},
-			);
+		return instance.put<UpdateTaskResponse>(`todo-lists/${task.todoListId}/tasks/${task.id}`,
+			model);
 	},
 };
